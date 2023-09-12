7d783e584ded1afa40c00d054575a6797f7b9594
Lei <1459703155@qq.com>
2023-09-12 15:57:50 +0800

    feat:选择排序

---
+++ src/main/java/org/wl/secondSection/_010_Selection.java
@@ -0,0 +1,34 @@
+package org.wl.secondSection;
+
+
+import org.wl.secondSection.sortTemplate.Example;
+
+/*
+选择排序：
+    首先，找到数组中最小的那个元素，其次，将它和数组的第 一个元素交换位置(如果第一个元素就是最小元素那么它就和自己交换)。
+    再次，在剩下的元素中 找到最小的元素，将它与数组的第二个元素交换位置。如此往复，直到将整个数组排序。
+    这种方法 叫做选择排序，因为它在不断地选择剩余元素之中的最小者。
+*/
+public class _010_Selection extends Example {
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exec(a, i, min);
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] a = new String[]{"b","s","d","a","j"};
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
---
+++ src/main/java/org/wl/secondSection/sortTemplate/Example.java
@@ -0,0 +1,32 @@
+package org.wl.secondSection.sortTemplate;
+
+/*排序模版*/
+public class Example {
+    public static void sort(Comparable[] a) {
+
+    }
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void exec(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void show(Comparable[] a) { // 在单行中打印数组
+        for (int i = 0; i < a.length; i++)
+            System.out.println(a[i] + " ");
+    }
+}
