4fdf4099940808a44b305dfd9a7f19ec3c3d24a5
Lei <1459703155@qq.com>
2023-09-16 23:27:43 +0800

    feat:顺序查找（基于无序链表）的符号表（字典)

---
+++ src/main/java/org/wl/thirdSection/_013_ST.java
@@ -0,0 +1,81 @@
+package org.wl.thirdSection;
+
+/*符号表（字典）*/
+public class _013_ST<Key extends Comparable<Key>, Value> {
+    public _013_ST() {
+    }
+
+    void put(Key key, Value value) {
+
+    }
+
+    Value get(Key key) {
+        return null;
+    }
+
+    void delete(Key key) {
+
+    }
+
+    boolean contains(Key key) {
+        return get(key) != null;
+    }
+
+    boolean isEmpty() {
+        return size() == 0;
+    }
+
+    int size() {
+        return 0;
+    }
+
+    int size(Key lo, Key hi) {
+        if (hi.compareTo(lo) < 0) return 0;
+        else if (contains(hi)) return rank(hi) - rank(lo) + 1;
+        else return rank(hi) - rank(lo);
+    }
+
+    Key min() {
+        return null;
+    }
+
+    Key max() {
+        return null;
+    }
+
+    /*小于等于 key 的最大键*/
+    Key floor(Key key) {
+        return null;
+    }
+
+    /*大于等于 key 的最小键*/
+    Key ceiling(Key key) {
+        return null;
+    }
+
+    /*小于 key 的键的数量*/
+    int rank(Key key) {
+        return 0;
+    }
+
+    /*排名为k的键*/
+    Key select(int k) {
+        return null;
+    }
+
+    void deleteMin() {
+        delete(min());
+    }
+
+    void deleteMax() {
+        delete(max());
+    }
+
+    Iterable<Key> keys() {
+        return null;
+    }
+
+    Iterable<Key> keys(Key lo, Key hi) {
+        return null;
+    }
+}
---
+++ src/main/java/org/wl/thirdSection/_014_SequentialSearchST.java
@@ -0,0 +1,116 @@
+package org.wl.thirdSection;
+
+import java.util.Iterator;
+
+/*顺序查找（基于无序链表）的符号表（字典）*/
+public class _014_SequentialSearchST<Key, Value> {
+    private Node first;
+
+    private int N = 0;
+
+    private class Node {
+        Key key;
+        Value value;
+        Node next;
+
+        public Node(Key key, Value value, Node next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public Value get(Key key) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                return x.value;
+            }
+        }
+        return null;
+    }
+
+    public void put(Key key, Value value) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.value = value;
+                return;
+            }
+        }
+        first = new Node(key, value, first);
+        N++;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void delete(Key key) {
+        for (Node x = first; x != null; x = x.next) {
+            if (x.next == null) {
+                if (key.equals(x.key)) {
+                    first = null;
+                    N--;
+                    return;
+                }
+            } else {
+                if (key.equals(x.next.key)) {
+                    x.next = x.next.next;
+                    N--;
+                    return;
+                }
+            }
+        }
+    }
+
+    public Iterable<Key> keys() {
+        return new KeysIterable();
+    }
+
+    private class KeysIterable implements Iterable<Key> {
+        @Override
+        public Iterator<Key> iterator() {
+            return new KeysIterator();
+        }
+    }
+
+    private class KeysIterator implements Iterator<Key> {
+
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Key next() {
+            Key key = current.key;
+            current = current.next;
+            return key;
+        }
+    }
+
+    public static void main(String[] args) {
+        _014_SequentialSearchST<String, Integer> searchST = new _014_SequentialSearchST<>();
+        searchST.put("a", 1);
+        searchST.put("b", 2);
+        searchST.put("c", 3);
+        searchST.put("a", 4);
+
+        for (String s : searchST.keys()) {
+            System.out.println(s);
+        }
+
+        System.out.println(searchST.get("a"));
+        System.out.println(searchST.size());
+
+        searchST.delete("a");
+        System.out.println(searchST.size());
+
+        searchST.delete("b");
+        System.out.println(searchST.size());
+
+        searchST.delete("c");
+        System.out.println(searchST.size());
+    }
+}
