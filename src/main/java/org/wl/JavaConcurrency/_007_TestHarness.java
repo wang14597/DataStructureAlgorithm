935cf5bfb2f303920bcf30e3fb9b7e501d93bf30
Lei <1459703155@qq.com>
2024-02-05 14:01:29 +0800

    feat: 使用CountDownLatch实现闭锁

---
+++ src/main/java/org/wl/JavaConcurrency/_007_TestHarness.java
@@ -0,0 +1,31 @@
+package org.wl.JavaConcurrency;
+
+import java.util.concurrent.CountDownLatch;
+
+// 使用CountDownLatch实现闭锁
+// 统计所有现在开始执行到所有线程执行完的耗时
+public class _007_TestHarness {
+    public long timeTasks(int nThreads, final Runnable task) throws InterruptedException {
+        final CountDownLatch startGate = new CountDownLatch(1);
+        final CountDownLatch endGate = new CountDownLatch(nThreads);
+
+        for (int i = 0; i < nThreads; i++) {
+            Thread t = new Thread(() -> {
+                try {
+                    startGate.await();
+                    try {
+                        task.run();
+                    } finally {
+                        endGate.countDown();
+                    }
+                } catch (InterruptedException ignored) {}
+            });
+            t.start();
+        }
+        long start = System.nanoTime();
+        startGate.countDown();
+        endGate.await();
+        long end = System.nanoTime();
+        return end - start;
+    }
+}
